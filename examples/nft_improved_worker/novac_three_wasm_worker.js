9b504e07cda3a25ad650b5a14950026cc0f64863
Manick94 <manick.be@hotmail.com>
2022-08-03 17:21:25 +0530

    mistakes were corrected and updated in novac js and html files

--- examples/nft_improved_worker/novac_three_wasm_worker.js
+++ examples/nft_improved_worker/novac_three_wasm_worker.js
@@ -2,88 +2,82 @@ var model;
 var clock = new THREE.Clock();
 var mixers = [];
 
-function isMobile(){
+function isMobile() {
     return /Android|mobile|iPad|iPhone/i.test(navigator.userAgent);
 }
 
-
-//interpolation factor
 var interpolationFactor = 24;
 
 var trackedMatrix = {
-    //for interpolation
+    // for interpolation
     delta: [
-        0,0,0,0,
-        0,0,0,0,
-        0,0,0,0,
-        0,0,0,0
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0
     ],
     interpolated: [
-        0,0,0,0,
-        0,0,0,0,
-        0,0,0,0,
-        0,0,0,0
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0,
+        0, 0, 0, 0
     ]
 }
-
-//declaring markers for NFT tracking
 
 var markers = {
     arveo_nft: {
         width: 1637,
         height: 2048,
-        dpi: 250,
-        url: "../DataNFT/arveo_nft/ARveo8",
-    },
+        dpi: 215,
+        url: "../examples/DataNFT/arveo_nft/ARveo8"
+    }
 };
 
-var setMatrix = function (matrix, value){
+var setMatrix = function (matrix, value) {
     var array = [];
     for (var key in value) {
         array[key] = value[key];
     }
     if (typeof matrix.elements.set === "function") {
         matrix.elements.set(array);
-    }else {
+    } else {
         matrix.elements = [].slice.call(array);
     }
 };
 
-//Var Worker -- Initializing the worker
-function start(container, marker, video, input_width, input_height, canvas_draw, render_update, track_update){
-    worker = new Worker('wasm_worker/artoolkit.wasm_worker.js');
-    worker.onmessage = function(ev) {
-        start2(container, marker, video, input_width, input_height, canvas_draw, render_update, track_update);
-    }
-}
-
-function start2(container, marker, video, input_width, input_height, canvas_draw, render_update, track_update){
+function start( container, marker, video, input_width, input_height, canvas_draw, render_update, track_update) {
     var vw, vh;
     var sw, sh;
     var pscale, sscale;
     var w, h;
     var pw, ph;
     var ox, oy;
-    var camera_para = './../../Data/camera_para-iphone 5 rear 640x480 1.0m.dat'
+    var worker;
+    var camera_para = "./../examples/Data/camera_para-iPhone 5 rear 640x480 1.0m.dat";
 
-    var canvas_process = document.createElement('canvas');
-    var context_process = canvas_process.getContext('2d');
+    var canvas_process = document.createElement("canvas");
+    var context_process = canvas_process.getContext("2d");
 
-    var renderer = new THREE.WebGLRenderer({canvas: canvas_draw, alpha: true, antialias: true});
+    // var context_draw = canvas_draw.getContext('2d');
+    var renderer = new THREE.WebGLRenderer({
+        canvas: canvas_draw,
+        alpha: true,
+        antialias: true
+    });
     renderer.setPixelRatio(window.devicePixelRatio);
 
     var scene = new THREE.Scene();
 
     var camera = new THREE.Camera();
     camera.matrixAutoUpdate = false;
+    // var camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+    // camera.position.z = 400;
 
     scene.add(camera);
 
-    //Adding an ambient light to the scene
     var light = new THREE.AmbientLight(0xffffff);
     scene.add(light);
 
-    //default sphere model to draw on NFT marker
     var sphere = new THREE.Mesh(
         new THREE.SphereGeometry(0.5, 8, 8),
         new THREE.MeshNormalMaterial()
@@ -92,31 +86,27 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
     var root = new THREE.Object3D();
     scene.add(root);
 
-
-    //Initializing the ARveo Model to draw on a nft marrker
-
-    // initialize the GLTF Loader
+    /* Load Model */
     var threeGLTFLoader = new THREE.GLTFLoader();
 
-    threeGLTFLoader.load("../Data/models/ARveo8.glb", 
-    function (gltf) {
-        model = gltf.scene.children[0];
-        model.position.z = 0;
-        model.position.x = 100;
-        model.position.y = 100;
+    threeGLTFLoader.load("../Data/models/Flamingo.glb", function (gltf) {
+            model = gltf.scene.children[0];
+            model.position.z = 0;
+            model.position.x = 100;
+            model.position.y = 100;
 
-        // var animation = gltf.animations[0];
-        // var mixer = new THREE.AnimationMixer(model);
-        // mixers.push(mixer);
-        // var action = mixer.clipAction(animation);
-        // action.play();
+            // var animation = gltf.animations[0];
+            // var mixer = new THREE.AnimationMixer(model);
+            // mixers.push(mixer);
+            // var action = mixer.clipAction(animation);
+            // action.play();
 
-        root.matrixAutoUpdate = false;
-        root.add(model);
-    }
+            root.matrixAutoUpdate = false;
+            root.add(model);
+        }
     );
 
-    var load = function(){
+    var load = function() {
         vw = input_width;
         vh = input_height;
 
@@ -130,7 +120,7 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
         container.style.width = sw + "px";
         container.style.height = sh + "px";
         canvas_draw.style.clientWidth = sw + "px";
-        canvas_draw.style.clientHeight = sh  + "px";
+        canvas_draw.style.clientHeight = sh + "px";
         canvas_draw.width = sw;
         canvas_draw.height = sh;
         w = vw * pscale;
@@ -140,11 +130,12 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
         ox = (pw - w) / 2;
         oy = (ph - h) / 2;
         canvas_process.style.clientWidth = pw + "px";
-        canvas_process.style.clientHight = ph + "px";
+        canvas_process.style.clientHeight = ph + "px";
         canvas_process.width = pw;
         canvas_process.height = ph;
-        
+
         renderer.setSize(sw, sh);
+
         worker = new Worker("../../js/artoolkit.worker.js");
 
         worker.postMessage({
@@ -157,11 +148,11 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
 
         worker.onmessage = function(ev) {
             var msg = ev.data;
-            switch (msg.type){
+            switch (msg.type) {
                 case "loaded": {
                     var proj = JSON.parse(msg.proj);
                     var ratioW = pw / w;
-                    var ratioH = pw / h;
+                    var ratioH = ph / h;
                     proj[0] *= ratioW;
                     proj[4] *= ratioW;
                     proj[8] *= ratioW;
@@ -174,12 +165,12 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
                     break;
                 }
 
-                case "endLoading" : {
-                    if(msg.end == true){
-                        //removing loader page if present
+                case "endLoading": {
+                    if (msg.end == true) {
+                        // removing loader page if present
                         var loader = document.getElementById('loading');
-                        if(loader) {
-                            loader.querySelector('.loading-text').innertext = 'Start the tracking!';
+                        if (loader) {
+                            loader.querySelector('.loading-text').innerText = 'Start the tracking!';
                             setTimeout(function(){
                                 loader.parentElement.removeChild(loader);
                             }, 2000);
@@ -204,8 +195,8 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
 
     var world;
 
-    var found = function(msg){
-        if(!msg){
+    var found = function(msg) {
+        if (!msg) {
             world = null;
         } else {
             world = JSON.parse(msg.matrixGL_RH);
@@ -221,17 +212,17 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
         context_process.drawImage(video, 0, 0, vw, vh, ox, oy, w, h);
 
         var imageData = context_process.getImageData(0, 0, pw, ph);
-        worker.postMessage({type: "process", imageData: imageData}, [
+        worker.postMessage({ type: "process", imagedata: imageData }, [
             imageData.data.buffer
         ]);
     }
 
-    var tick = function(){
+    var tick = function() {
         draw();
-        // requestAnimationFrame(tick);
+        requestAnimationFrame(tick);
 
-        // if(mixers.length > 0){
-        //     for  (var i = 0; mixers.length; i++){
+        // if (mixers.length > 0) {
+        //     for (var i = 0; i < mixers.length; i++) {
         //         mixers[i].update(clock.getDelta());
         //     }
         // }
@@ -244,20 +235,22 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
         time += dt;
         lasttime = now;
 
-        if(!world) {
+        if (!world) {
             root.visible = false;
         } else {
             root.visible = true;
 
-            //interpolate matrix
-            for (var i =o; i < 16; i++){
+            // interpolate matrix
+            for (var i = 0; i < 16; i++) {
                 trackedMatrix.delta[i] = world[i] - trackedMatrix.interpolated[i];
-                trackedMatrix.interpolated[i] = trackedMatrix.interpolated[i] + trackedMatrix.delta[i] / interpolationFactor;
+                trackedMatrix.interpolated[i] =
+                    trackedMatrix.interpolated[i] +
+                    trackedMatrix.delta[i] / interpolationFactor;
             }
 
-            //set matrix of root by detected 'world' matrix
+            // set matrix of 'root' by detected 'world' matrix
             setMatrix(root.matrix, trackedMatrix.interpolated);
-        } 
+        }
 
         renderer.render(scene, camera);
     };
@@ -265,5 +258,4 @@ function start2(container, marker, video, input_width, input_height, canvas_draw
     load();
     tick();
     process();
-
 }
